01ef41eb7777771c2e5bda9ea7f06c076bccc1e9
Miguel <mcastillo.ext@clorian.com>
2025-03-10 15:51:23 -0300

    updated minor version, added update function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,9 +7,11 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             #[cfg(desktop)]
-            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
