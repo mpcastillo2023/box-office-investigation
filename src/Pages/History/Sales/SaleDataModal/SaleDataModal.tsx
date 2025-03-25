29727baec7b4ff1076594c7870698cf65f6733e7
eduardo lara <elara.ext@clorian.com>
2025-03-25 13:38:36 -0300

    translations update

--- src/Pages/History/Sales/SaleDataModal/SaleDataModal.tsx
+++ src/Pages/History/Sales/SaleDataModal/SaleDataModal.tsx
@@ -78,7 +78,7 @@ export default function SaleItem({
   return (
     <>
       <Modal
-        withCloseButton={false}
+        withCloseButton={true}
         setIsModalOpen={setSaleDataModalOpen}
         isModalOpen={isSaleDataModalOpen}
         variant="success"
